50c2b946f62d9587988a690f2af980ebd51e7336
Strandgaard96 <magnus.str@gmail.com>
2024-12-11 10:41:12 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Clone the repository
+git clone https://github.com/AlanKerstjens/MoleculeAutoCorrect.git
+git clone https://github.com/AlanKerstjens/Molpert.git
+
+# Install molpert
+export MOLPERT="$(pwd)/Molpert"
+mkdir ${MOLPERT}/build && cd ${MOLPERT}/build
+cmake ..
+make install
+
+# Set the MOLECULE_AUTO_CORRECT environment variable
+export MOLECULE_AUTO_CORRECT="$(pwd)/MoleculeAutoCorrect"
+
+# Create the build directory and navigate into it
+mkdir -p "${MOLECULE_AUTO_CORRECT}/build"
+cd "${MOLECULE_AUTO_CORRECT}/build"
+
+# Run CMake with the given include directory (Ensure $MOLPERT is set before running this script)
+cmake -DMolpert_INCLUDE_DIRS="${MOLPERT}/source" ..
+
+# Build and install MoleculeAutoCorrect
+make install
